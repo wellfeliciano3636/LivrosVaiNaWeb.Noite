a9fb4402876da17972b1388a2d90ee1b0297bd9e
Your Name <you@example.com>
2024-11-27 20:28:04 -0300

    adicionando components quero doar

--- src/Components/Header/footer/Footer.jsx
+++ src/Components/Header/footer/Footer.jsx
@@ -1,9 +1,26 @@
 import s from './footer.module.scss'
+import logoFacebook from '../../../assets/logoFacebook.png'
+import logoTwitter from '../../../assets/logoTwitter.png'
+import logoYoutube from '../../../assets/logoYoutube.png'
+import logoLinkedin from '../../../assets/logoLinkedin.png'
+import logoInstagran from '../../../assets/logoInstagran.png'
 
 export default function Footer (){
     return(
-       <footer>
-            
+       <footer className={s.footer}>
+            <section className={s.contatosFooter}>
+                <p>83991562578</p>
+                <nav>
+                    <a href=""><img src={logoFacebook} alt="Imagem da logo facebook" /></a>
+                    <a href=""><img src={logoTwitter} alt="Imagem da logo Twitter" /></a>
+                    <a href=""><img src={logoYoutube} alt="Imagem da logo Youtube" /></a>
+                    <a href=""><img src={logoLinkedin} alt="Imagem da logo Linkedin" /></a>
+                    <a href=""><img src={logoInstagran} alt="Imagem da logo Instagran" /></a>
+                </nav>
+                <section className={s.copyright}>
+                    <p>layt</p>
+                </section>
+            </section>
        </footer>
     )
 }
--- src/pages/QueroDoar/QueroDoar.jsx
+++ src/pages/QueroDoar/QueroDoar.jsx
@@ -1,5 +1,22 @@
+import iconeLivro from '../../assets/iconeLivro.png'
+import s from './queroDoar.module.scss'
+
 export default function QueroDoar(){
     return(
-        <h2>eu sou o componente quero doar</h2>
+        <section>
+            <p>Por favor preencha o formulario com suas infornaçoes e as infornaçoes do Livros.  </p>
+            <form action="">
+                <div>
+                    <img src={iconeLivros} alt="Imagen com icone de livro com bordas" />
+                    <h2>infornaçoes do Livros</h2>
+                </div>
+                    <input type="text" name="" id="" placeholder='Titulo'/>
+                    <input type="text" name="" id="" placeholder='Categoria'/>
+                    <input type="text" name="" id="" placeholder='Autor'/>
+                    <input type="text" name="" id="" placeholder='link da imagem'/>
+                    <input type="submit" volve="Doar" className={s.buttonDoar}/>
+
+            </form>
+        </section>
     )
 }
